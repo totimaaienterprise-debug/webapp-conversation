4a27d1dd4ba239c9ebef6676069301aaad8342d9
totimaaienterprise-debug <totimaai.enterprise@gmail.com>
2025-11-09 16:27:03 +0900

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -10,35 +10,40 @@ export interface IHeaderProps {
   isMobile?: boolean
   onShowSideBar?: () => void
   onCreateNewChat?: () => void
+  iconUrl?: string  
 }
 const Header: FC<IHeaderProps> = ({
   title,
   isMobile,
   onShowSideBar,
   onCreateNewChat,
+  iconUrl,
 }) => {
+  const showSidebarToggle = isMobile && !!onShowSideBar
+  const showNewChat = isMobile && !!onCreateNewChat
+  
   return (
-    <div className="shrink-0 flex items-center justify-between h-12 px-3 bg-gray-100">
-      {isMobile
-        ? (
-          <div
-            className='flex items-center justify-center h-8 w-8 cursor-pointer'
-            onClick={() => onShowSideBar?.()}
-          >
-            <Bars3Icon className="h-4 w-4 text-gray-500" />
-          </div>
-        )
-        : <div></div>}
+    <div className="relative shrink-0 flex items-center justify-center h-12 px-3 bg-gray-100">
+      {showSidebarToggle && (
+        <div
+          className='flex items-center justify-center h-8 w-8 cursor-pointer absolute left-3'
+          onClick={() => onShowSideBar?.()}
+        >
+          <Bars3Icon className="h-4 w-4 text-gray-500" />
+        </div>
+      )}    
       <div className='flex items-center space-x-2'>
-        <AppIcon size="small" />
+        <AppIcon size="small" rounded icon={iconUrl} />
         <div className=" text-sm text-gray-800 font-bold">{title}</div>
       </div>
-      {isMobile
-        ? (
-          <div className='flex items-center justify-center h-8 w-8 cursor-pointer' onClick={() => onCreateNewChat?.()} >
-            <PencilSquareIcon className="h-4 w-4 text-gray-500" />
-          </div>)
-        : <div></div>}
+      {showNewChat && (
+        <div
+          className='flex items-center justify-center h-8 w-8 cursor-pointer absolute right-3'
+          onClick={() => onCreateNewChat?.()}
+        >
+          <PencilSquareIcon className="h-4 w-4 text-gray-500" />
+        </div>
+      )}
     </div>
   )
 }
