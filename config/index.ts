79fb49b142c11168ab1f5ca63ddced0074ed2938
totimaaienterprise-debug <totimaai.enterprise@gmail.com>
2025-11-09 15:09:13 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,8 @@ import type { AppInfo } from '@/types/app'
 export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+const disableSessionSameSiteEnv = process.env.NEXT_PUBLIC_DISABLE_SESSION_SAME_SITE
+
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
   description: '',
