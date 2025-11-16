078cd2d32cd9c2f8e805ce367a57ae87112ad562
totimaaienterprise-debug <totimaai.enterprise@gmail.com>
2025-11-16 16:10:20 +0900

    Update styling for question component

--- app/components/chat/question/index.tsx
+++ app/components/chat/question/index.tsx
@@ -18,7 +18,7 @@ const Question: FC<IQuestionProps> = ({ id, content, useCurrentUserAvatar, imgSr
       <div>
         <div className={`${s.question} relative text-sm text-gray-900`}>
           <div
-            className={'mr-2 py-3 px-4 bg-blue-500 rounded-tl-2xl rounded-b-2xl'}
+            className={'mr-2 py-4 px-5 text-white bg-gradient-to-l from-blue-600 via-sky-500 to-cyan-400 rounded-tl-2xl rounded-b-2xl shadow-[0_15px_35px_rgba(15,23,42,0.25)]'}
           >
             {imgSrcs && imgSrcs.length > 0 && (
               <ImageGallery srcs={imgSrcs} />
